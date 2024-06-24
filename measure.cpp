5b3a7db1294523340f3f50636bd7aec3ecb94a1c
Clement Buon <aubanel.buon@gmail.com>
2024-06-24 15:59:38 +0200

    embedded measure in c++ local library

---
+++ measure.cpp
@@ -0,0 +1,57 @@
+#include "measure.hpp"
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+double get_measure(std::vector<double> spectre, double delta, double x, int i) {
+    std::sort(spectre.begin(), spectre.end());
+    Circle<double> c = Circle<double>(spectre, x, i);
+    couple ind_end = c.get_start();
+    couple ind_start = ind_end;
+    int d = c.get(ind_end) - delta;
+    for (int i = 0; i < c.length() - 1; i++) {
+        ind_start = c.get_previous(ind_start);
+        if (fabs(c.get(ind_start) - d) > delta) {
+            ind_start = c.get_next(ind_start);
+            break;
+        }
+    }
+    double p = (double)c.get_cardinal(ind_start, ind_end) / (double)c.length();
+    double S = p;
+
+    while (true) {
+        int d1 = c.get(ind_start) + delta; // Next start index change
+        int d2 = c.get(c.get_next(ind_end)) - delta; // Next end index change
+        int next_d = fmin(d1, d2);
+
+        if (d1 <= d2) {
+            p -= (double)c.get_count(ind_start) / (double)c.length();
+        }
+        if (d2 <= d1) {
+            p += (double)c.get_count(ind_end) / (double)c.length();
+        }
+        S = fmax(S, p);
+        d = next_d;
+
+        if (d1 <= d2) {
+            ind_start = c.get_next(ind_start);
+            if (couple_eq(ind_start, ind_end)) {
+                ind_end = ind_start;
+            }
+        }
+
+        if (d2 <= d1) {
+            ind_end = c.get_next(ind_end);
+        }
+
+        if (c.is_start(ind_end)) {
+            return S;
+        }
+    }
+}
+
+int main() {
+    std::vector<double> spectre = {1., 1., 1., 1., 1., 1., 1.2};
+    std::cout << get_measure(spectre, 0.15, 1, 0);
+    return 0;
+}
---
+++ measure.hpp
@@ -0,0 +1,96 @@
+#include <vector>
+
+using couple = std::pair<int, int>;
+
+bool couple_eq(couple c1, couple c2) {
+    auto [a, b] = c1;
+    auto [c, d] = c2;
+    return (a == c) && (b == d);
+}
+
+template <typename T>
+class Circle {
+    std::vector<T> t;
+    std::vector<int> count;
+    double x;
+    int len;
+    couple start;
+
+    public:
+    Circle(std::vector<T> spectre, double x, int i) : t(std::vector<double>(spectre)), count(std::vector<int>()), x(x), len(t.size()), start(couple(i, 0)) {
+        count.push_back(1);
+        int ind = 0;
+        while (ind + 1 < t.size()) {
+            if (t[ind] == t[ind + 1]) {
+                t.erase(t.begin() + ind+1);
+                count[count.size() - 1] += 1;
+            } else {
+                count.push_back(1);
+                ind++;
+            }
+        }
+    }
+
+    bool is_start(couple end) {
+        auto [i, k] = start;
+        auto [j, l] = end;
+        return i == j;
+    }
+
+    couple get_start() {
+        return start;
+    }
+
+    double get(couple a) {
+        auto [i, k] = a;
+        return t[i] + x*k;
+    }
+
+    couple get_next(couple a) {
+        auto [i, k] = a;
+        if (i + 1 == t.size()) {
+            return couple(0, k+1);
+        }
+        return couple(i+1, k);
+    }
+
+    couple get_previous(couple a) {
+        auto [i, k] = a;
+        if (i == 0) {
+            return couple(t.size() - 1, k-1);
+        }
+        return couple(i-1, k);
+    }
+
+    int get_count(couple a) {
+        auto [i, _] = a;
+        return count[i];
+    }
+
+    int get_cardinal(couple a1, couple a2) {
+        auto [i1, k1] = a1;
+        auto [i2, k2] = a2;
+        int s = 0;
+
+        if (k1 == k2) {
+            for (int i = 0; i < t.size(); i++) {
+                int c = fmax(0, k2-k1-1);
+                c += (i >= i1 && i <= i2);
+                s += count[i] * c;
+            }
+        } else {
+            for (int i = 0; i < t.size(); i++) {
+                int c = fmax(0, k2-k1-1);
+                c += (i >= i1 || i <= i2);
+                s += count[i] * c;
+            }
+        }
+        return s;
+    }
+
+    int length() {
+        return len;
+    }
+};
+
+double get_measure(std::vector<double> spectre, double delta, double x, int i);
