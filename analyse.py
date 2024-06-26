3b1dc96c56b2409a22ade95e40478d2932816a02
Clement Buon <aubanel.buon@gmail.com>
2024-06-26 13:49:07 +0200

    optimised python-wise

--- analyse.py
+++ analyse.py
@@ -3,9 +3,13 @@ from large_et_al import measure
 
 NB_LINE = 1018
 
-#data = load_pick("measure.pick")
-#already_done = load_done("performance_test.txt")
+data = load_pick("measure.pick")
+already_done = load_done("performance_test.txt")
 t = Timer(NB_LINE, msg="Saving data :")
+for d in data[len(already_done):]:
+    save(d[0], (measure(d[1:], 0.075),), path="performance_test.txt")
+    t.update()
 
-exec_from_txt("measure.txt", lambda x, y : save(x, (measure(y, 0.075),), path="performance_test.txt"), t)
-# 16:00
+#exec_from_txt("measure.txt", lambda x, y : save(x, (measure(y, 0.075),), path="performance_test.txt"), t, already_done=already_done)
+# 16:00 - 18h10
+# 11:52 - 11h54
