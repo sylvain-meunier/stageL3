5fb5702939483be0ac045ed306fe236ed95b2fa4
Clement Buon <aubanel.buon@gmail.com>
2024-07-24 14:09:42 +0200

    generated examples

--- plot.py
+++ plot.py
@@ -1,6 +1,8 @@
 import numpy as np
 from quantization import find_local_minima, error
 import matplotlib.pyplot as plt
+from util import EPSILON
+import matplotlib.pylab as pylab
 
 def f(a, t) :
     return t/a - int(t/a)
@@ -45,4 +47,47 @@ def plot2():
     plt.plot(x, y, '.', markersize=2)
     plt.show()
 
-plot2()
+def canonical(score, perf, n, default=120):
+    if (perf[n+1] - perf[n]) > EPSILON:
+        return 60 * (score[n+1] - score[n]) / (perf[n+1] - perf[n])
+    return default
+
+def th_delta(t1, t2):
+    return abs(1 / t1 - 1/t2)
+
+
+def plot_castle():
+    #plt.rcParams['axes.facecolor'] = 'gray'
+
+    size = 35
+    params = {'legend.fontsize': size,
+        'axes.labelsize': size,
+        'axes.titlesize':size,
+        'xtick.labelsize':20,
+        'ytick.labelsize':20}
+    pylab.rcParams.update(params)
+
+    perf = [1.757, 2.330, 2.534, 2.924, 3.296, 3.669, 4.080, 4.649, 4.846, 5.274, 5.650, 6.013, 6.424]
+    t1 = [1, 2.5, 3, 4, 5, 6, 7, 8.5, 9, 10, 11, 12, 13]
+    t2 = [1.1875, 2.775, 3.25, 4.125, 5.25, 6.25, 7.45, 9, 9.375, 10.416667, 11.375, 12.375, 13]
+    c1 = []
+    c2 = []
+
+    for i in range(len(perf) - 1):
+        c1.append(canonical(t1, perf, i))
+        c2.append(canonical(t2, perf, i))
+
+    dist = [abs(np.log(c1[i] / c2[i])) for i in range(len(c1))]
+
+    
+    #plt.plot(perf[:-1], c1, '*', color="white", markersize=8, label="Tempo curve A")
+    #plt.plot(perf[:-1], c2, '.', color="black", markersize=8, label="Tempo curve B")
+    #plt.plot(perf[:-1], dist, '-', color="black", markersize=8, label="Tempo distance")
+    #plt.plot((perf[0], perf[-2]), [EPSILON, EPSILON], '-', color="red", label="Human ability treshold")
+    plt.plot(perf[:-1], [th_delta(c1[i], c2[i]) for i in range(len(c1))], '--', color="black", markersize=8, label="Theorical distance")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Tempo distance (log)")
+    plt.legend()
+    plt.show()
+
+plot_castle()
