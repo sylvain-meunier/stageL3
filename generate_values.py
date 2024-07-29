9703419d56a41f4c1949eac880aaf5e5df2a7270
Clement Buon <aubanel.buon@gmail.com>
2024-07-29 18:24:53 +0200

    patched a bug when generating data

--- generate_values.py
+++ generate_values.py
@@ -1,20 +1,16 @@
 import numpy as np
-from large_et_al import *
+from large_et_al import Estimator, RandomEstimator, TempoTracker, measure, normalize_tempo
 from util import get_matching_from_txt, fit_matching, find_recursive, path, EPSILON
 from pic import load_done, save, Timer, txt_to_pickle
 import matplotlib.pyplot as plt
 
-save_file = "random_measure.txt"
+save_file = "measure.txt"
 
-init_bpm = 120
-tt = TempoTracker(RandomEstimator(), tempo_init=init_bpm)
-
+init_bpm = 40
 l = []
 find_recursive(l, path, rec=True)
-ratio = []
 nb_piece = 0
 already_done = load_done(save_file)
-results = ([], [], [])
 
 timer = Timer(1018, current=len(already_done))
 
@@ -28,21 +24,24 @@ for perfo in l:
     except:
         continue
 
+    tt = TempoTracker(Estimator(accuracy=250), init_bpm)
+    results = ([], [], [])
+
     for ti in range(len(inputs)-1):
         beat_input, time_input = inputs[ti]
         results[0].append(time_input)
+        results[1].append(((inputs[ti+1][0] - beat_input) * 60 / (inputs[ti+1][1] - time_input)))
+        results[2].append(tt.update_and_return_tempo(time_input))
 
-        if inputs[ti+1][1] - time_input > EPSILON:
-            results[1].append(((inputs[ti+1][0] - beat_input) * 60 / (inputs[ti+1][1] - time_input)))
-            results[2].append(tt.update_and_return_tempo(time_input, debug=0))
-
-    tmp = [normalize_tempo(i, min=1, max=2) for i in np.array(results[2][1:]) / np.array(results[1][:-1])]
-    save(perfo, tmp, path=save_file)
+    tmp = results[2]
+    tmp2 = results[1]
+    tmp = [normalize_tempo(tmp[i+1] / tmp2[i]) for i in range(len(tmp2) - 1)]
+    #save(perfo, tmp, path=save_file)
+    if "11-3" in perfo:
+        plt.hist(tmp, bins=100)
+        plt.title(str(perfo) + " " + str(measure(tmp, 0.075))[:8])
+        plt.show()
+        exit(0)
     timer.update()
-
-plt.yscale("linear")
-plt.hist(ratio, bins=100)
-plt.title("A view of Random TempoTracker performance for the whole (n)-ASAP dataset (" + str(nb_piece) + " pieces)")
-plt.show()
 
 txt_to_pickle(path=save_file)
--- new_large_test.py
+++ new_large_test.py
@@ -1,12 +1,13 @@
 import os
 import numpy as np
-from util import EPSILON, get_beats_from_txt, get_matching_from_txt, fit_matching
+from util import EPSILON, get_beats_from_txt, get_matching_from_txt, fit_matching, path, find_recursive
 import matplotlib.pyplot as plt
 from large_score import Large, TimeKeeper, LargeKeeper
-from large_et_al import QuantiTracker, Estimator, RandomEstimator, TempoTracker
+from large_et_al import QuantiTracker, Estimator, RandomEstimator, TempoTracker, measure
 from plot import biglabels
+from pic import load_done, save, Timer, txt_to_pickle
 
-init_bpm = 40
+init_bpm = 100
 tt = QuantiTracker(init_bpm)
 tk = TimeKeeper(init_bpm, beta=0.06, alpha=0.08)
 large = Large(init_bpm, eta_p=0.7, eta_phi=4) # 0.675, 0.65, 0.6
@@ -15,10 +16,8 @@ e = TempoTracker(Estimator(accuracy=250), init_bpm)
 # (0.6, 3.14)
 # (0.8, 2)
 
-inputs = []
-results = [[], [], [], [], [], [], [], [], []]
+save_file = "random_measure.txt"
 
-path = "./ASAP/"
 folder_path = "Prokofiev/Toccata/"
 folder_path = "Brahms/Six_Pieces_op_118/2/"
 folder_path = "Schumann/Toccata/"
@@ -27,34 +26,15 @@ folder_path = "Beethoven/Piano_Sonatas/18-2/"
 folder_path = "Mozart/Piano_sonatas/11-3/"
 folder_path = "Ravel/Pavane/"
 folder_path = "Bach/Italian_concerto/"
+folder_path = "Ravel/Pavane/"
 folder_path = "Balakirev/Islamey/"
-folder_path = "Ravel/Miroirs/3_Une_Barque/"
 folder_path = "Mozart/Piano_sonatas/11-3/"
 
-
 l = []
+find_recursive(l, path, rec=1)
+already_done = load_done(save_file)
 
-def find_recursive(current_path, rec=False):
-    listdir = os.listdir(current_path)
-    midi = False
-    for f in listdir:
-        if ".mid" in f:
-            midi = True
-            f = f.split('.')
-            if f[-1] != "mid" or "midi_score" in f[0]:
-                continue
-            if f[0] + ".match" in listdir:
-                l.append(current_path + '/' + f[0])
-    if rec and not midi:
-        for f in listdir:
-            if not "." in f:
-                find_recursive(current_path + '/' + f, rec=rec)
-
-find_recursive(path + folder_path, rec=1)
-#inputs = get_beats_from_txt(l[0] + "_annotations.txt", accept_br=1)
-inputs = [(ind, inputs[ind]) for ind in range(len(inputs))]
-
-already_done = []
+timer = Timer(1018, current=len(already_done))
 
 def normalize_tempo(a, x=1):
     return np.exp(np.log(a / x) - np.log(2) * np.floor(np.log2(a / x))) * x
@@ -62,23 +42,27 @@ def normalize_tempo(a, x=1):
 #plt.rcParams['axes.facecolor'] = 'gray'
 biglabels()
 
-for perfo in l[:1]:
+for perfo in l:
     if perfo in already_done:
         continue
     try:
         matching = get_matching_from_txt(perfo + ".mid")
         inputs = fit_matching(matching, unit="quarter")
         print(perfo)
+        if len(inputs) == 0:
+            continue
     except Exception as e:
         continue
 
-    for ti in range(len(inputs)-2):
-        beat_input, time_input = inputs[ti]
-        
-        if time_input > 1000:
-            break
 
-        results[0].append(time_input)
+    e = TempoTracker(RandomEstimator(accuracy=250), init_bpm)
+    results = [[], [], [], [], [], [], [], [], []]
+    timer.update()
+
+    for ti in range(len(inputs)-1):
+        beat_input, time_input = inputs[ti]
+
+        #results[0].append(time_input)
         results[5].append(((inputs[ti+1][0] - beat_input) * 60 / (inputs[ti+1][1] - time_input)))
 
         #results[6].append(tt.update_and_return_tempo(time_input, debug=1))
@@ -94,16 +78,20 @@ for perfo in l[:1]:
     #plt.plot(results[0], results[3], '*', markersize=2, color="red", label='LargeKeeper tempo')
     #plt.plot(results[0], results[6], '--', markersize=4, color="black", label='Quantified tempo')
     tmp = results[2]
-    tmp = [normalize_tempo(results[2][i]) for i in range(len(results[0]))]
-    tmp2 = [normalize_tempo(results[5][i]) for i in range(len(results[0]))]
+    tmp2 = results[5]
+    #tmp = [normalize_tempo(results[2][i]) for i in range(len(results[0]))]
+    #tmp2 = [normalize_tempo(results[5][i]) for i in range(len(results[0]))]
     #plt.plot(results[0], tmp2, '*', markersize=6, color="blue", label='Canonical tempo', alpha=0.5)
     #plt.plot(results[0][:-1], tmp[1:], '.', markersize=6, color="black", label='Naive estimator')
-    tmp = [normalize_tempo(tmp[i+1] / tmp2[i]) for i in range(len(tmp2) - 2)]
-    plt.hist(tmp, bins=100)
+    tmp = [normalize_tempo(tmp[i+1] / tmp2[i]) for i in range(len(tmp2) - 1)]
+    save(perfo, tmp, path=save_file)
+
     #plt.plot(results[0], results[2], '--', markersize=2, color="cyan", label='TimeKeeper tempo')
     #plt.plot(results[0], results[5], '.', markersize=2, label='T∗(t)', color="yellow", alpha=0.5)
     #plt.title("Tempo curve for a performance of Islamey, Op.18, M. Balakirev with naive algorithm")
     #plt.xlabel('Time (second)')
     #plt.ylabel('Normalized tempo')
     #plt.legend()
-    plt.show()
+    #plt.show()
+
+txt_to_pickle(path=save_file)
